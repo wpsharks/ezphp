04f84e665371df0556d47e72e51261d05ab4026a
JasWSInc <jason@websharks-inc.com>
2013-09-22 08:53:06 -0800

    Adding support for fake PHP tags; e.g. <!php !>.

--- ezphp/ezphp.php
+++ ezphp/ezphp.php
@@ -14,31 +14,34 @@ if(!defined('WPINC'))
 if(!defined('EZPHP_EXCLUDED_POST_TYPES'))
 	define('EZPHP_EXCLUDED_POST_TYPES', '');
 
-add_action('init', 'ezphp::init', 1);
-
 class ezphp
-	{
-		public static function init()
-			{
-				add_filter('the_content', 'ezphp::filter', 1);
-				add_filter('get_the_excerpt', 'ezphp::filter', 1);
-				add_filter('widget_text', 'ezphp::evaluate', 1);
-			}
+{
+	public static function init()
+		{
+			add_filter('the_content', 'ezphp::filter', 1);
+			add_filter('get_the_excerpt', 'ezphp::filter', 1);
+			add_filter('widget_text', 'ezphp::evaluate', 1);
+		}
 
-		public static function filter($content_excerpt)
-			{
-				$excluded_post_types = preg_split('/[\s;,]+/', EZPHP_EXCLUDED_POST_TYPES, NULL, PREG_SPLIT_NO_EMPTY);
+	public static function filter($content_excerpt)
+		{
+			$excluded_post_types = preg_split('/[\s;,]+/', EZPHP_EXCLUDED_POST_TYPES, NULL, PREG_SPLIT_NO_EMPTY);
 
-				if(isset($GLOBALS['post']->post_type) && !in_array($GLOBALS['post']->post_type, $excluded_post_types, TRUE))
-					return ezphp::evaluate($content_excerpt);
+			if(isset($GLOBALS['post']->post_type) && !in_array($GLOBALS['post']->post_type, $excluded_post_types, TRUE))
+				{
+					$content_excerpt = preg_replace(array('/\<\!php(\s+)/', '/(\s+)\!\>/'), // Fake PHP tags.
+					                                array('<?php${1}', '${1}?>'), // Real tags now.
+					                                ezphp::evaluate($content_excerpt));
+				}
+			return $content_excerpt; // That was ez :-)
+		}
 
-				return $content_excerpt;
-			}
+	public static function evaluate($string)
+		{
+			ob_start(); // Output buffer.
+			eval('?>'.trim((string)$string).'<?php ');
+			return ob_get_clean();
+		}
+}
 
-		public static function evaluate($string)
-			{
-				ob_start();
-				eval('?>'.trim((string)$string).'<?php ');
-				return ob_get_clean();
-			}
-	}
+add_action('init', 'ezphp::init', 1);
